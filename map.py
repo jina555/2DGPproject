c77fe905644519e548e6af53411af3014688539f
jina555 <kja390@tukorea.ac.kr>
2025-10-27 19:44:28 +0900

    map 추가

--- map.py
+++ map.py
@@ -4,11 +4,14 @@ GROUND_Y=80
 
 class Map:
     def __init__(self):
-        pass
+        self.image=load_image('map1.png')
+        self.w=self.image.w
+        self.h=self.image.h
 
     def draw(self):
-        w=get_canvas_width()
-        draw_rectangle(0,GROUND_Y-15,w,GROUND_Y-15)
+        cw=get_canvas_width()
+        ch=get_canvas_height()
+        self.image.clip_draw(0,0,self.w,self.h,cw//2,ch//2,cw,ch)
 
     def update(self):
         pass
