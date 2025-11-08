c6923c131bf062fc11715bdd9a9ecef309cc899a
jina555 <kja390@tukorea.ac.kr>
2025-11-08 15:57:42 +0900

    코드 뼈대 작성

---
+++ start_mode.py
@@ -0,0 +1,25 @@
+from pico2d import *
+import game_framework
+import play_mode
+import random
+
+CANVAS_WIDTH=1280
+CANVAS_HEIGHT=720
+
+
+background=None
+
+def init():
+    pass
+def finish():
+    pass
+def handle_event():
+    pass
+def update():
+    pass
+def draw():
+    pass
+def pause():
+    pass
+def resume():
+    pass
