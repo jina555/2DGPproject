05d96ad79ada53e3e88d69171905f6922342ef4e
jina555 <kja390@tukorea.ac.kr>
2025-11-10 00:23:44 +0900

    UIManager 기본 뼈대 작성

---
+++ ui_manager.py
@@ -0,0 +1,15 @@
+from pico2d import *
+import game_world
+
+CANVAS_WIDTH=1280
+CANVAS_HEIGHT=725
+
+class UIManager:
+    def __init__(self):
+        pass
+    def calculate_rect(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
