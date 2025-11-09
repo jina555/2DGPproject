0bd1d7f71c65b2a2b29584196944f25e84895f5a
jina555 <kja390@tukorea.ac.kr>
2025-11-10 00:42:44 +0900

    inventory 생성 코드 추가

--- play_mode.py
+++ play_mode.py
@@ -4,29 +4,40 @@ from map import Map
 from grass import Grass
 import game_framework
 import game_world
+from ui_manager import UIManager
+
+CANVAS_WIDTH=1280
+CANVAS_HEIGHT=725
+
 
 
 player= None
 grass_map=None
 grass=None
+ui_manager=None
 
 
 def init():
-    global player, game_map, grass
+    global player, game_map, grass,ui_manager
 
     game_map = Map()
     player = Character()
     grass=Grass()
+    ui_manager=UIManager()
+
     game_world.add_object(game_map,0)
     game_world.add_object(grass,1)
     game_world.add_object(player,1)
+    game_world.add_object(ui_manager,2)
+
     pass
 def finish():
-    global player,game_map,grass
+    global player,game_map,grass,ui_manager
     game_world.clear()
     player=None
     game_map=None
     grass=None
+    ui_manager=None
 
 
 def handle_events():
--- ui_manager.py
+++ ui_manager.py
@@ -4,12 +4,26 @@ import game_world
 CANVAS_WIDTH=1280
 CANVAS_HEIGHT=725
 
+BAG_ICON_X = 60
+BAG_ICON_Y = 60
+BAG_ICON_W, BAG_ICON_H=70,70
+BAG_SCALE_NORMAL=1.0
+BAG_SCALE_HOVER=1.1
+
 class UIManager:
     def __init__(self):
+        self.bag_icon_image=load_image('가방.png')
+        self.inventory_image=load_image('inventory.png')
+
+        self.bag_icon_scale=BAG_SCALE_NORMAL
+
         pass
     def calculate_rect(self):
         pass
     def update(self):
         pass
     def draw(self):
+        current_w=BAG_ICON_W * self.bag_icon_scale
+        current_h=BAG_ICON_H * self.bag_icon_scale
+        self.bag_icon_image.draw(BAG_ICON_X,BAG_ICON_Y,current_w,current_h)
         pass
