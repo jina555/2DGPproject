b323593f885a775a17976a528056901364694f0d
jina555 <kja390@tukorea.ac.kr>
2025-11-10 16:02:40 +0900

    몬스터 class 뼈대 생성

---
+++ monster.py
@@ -0,0 +1,19 @@
+from pico2d import *
+import game_framework
+import game_world
+from state_machine import StateMachine, State
+
+class Monster:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def get_bb(self):
+        pass
+    def handle_collision(self,group,other):
+        pass
+    def remove_self(self):
+        pass
