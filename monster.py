dfeff122dd5f849912dfbc19881d8e044aafd616
jina555 <kja390@tukorea.ac.kr>
2025-11-10 16:58:56 +0900

    몬스터 walk 상태 코드 작성, 5마리 생성 코드 추가

--- monster.py
+++ monster.py
@@ -2,22 +2,86 @@ from pico2d import *
 import game_framework
 import game_world
 from state_machine import StateMachine, State
-W,H=20,20
+import random
+
+WALK_FRAMES_PER_ACTION=5
+WALK_TIME_PER_ACTION=1.0
+WALK_ACTION_PER_TIME=1.0/WALK_FRAMES_PER_ACTION
+
+W,H=32,32
+MOVE_SPEED=150
 class Walk(State):
+    def __init__(self,p):
+        self.p=p
+        pass
+    def enter(self,e):
+        self.p.vx= MOVE_SPEED * self.p.face_dir
+        pass
+    def exit(self,e):
+        pass
+    def do(self):
+        self.p.x +=self.p.vx*game_framework.frame_time
+        self.p.frame=(self.p.frame+WALK_FRAMES_PER_ACTION * WALK_ACTION_PER_TIME *game_framework.frame_time) % WALK_FRAMES_PER_ACTION
+
+        if self.p.x < 50:
+            self.p.vx=MOVE_SPEED
+            self.p.face_dir=1
+        elif self.p.x > 1280-50:
+            self.p.vx=-MOVE_SPEED
+            self.p.face_dir=-1
+        pass
+    def draw(self):
+        if self.p.face_dir==1:
+            self.p.img_walk.clip_composite_draw(int(self.p.frame)*W,0,W,H,0,'h',self.p.x,self.p.y,W*3,H*3)
+        else:
+            self.p.img_walk.clip_draw(int(self.p.frame)*W,0,W,H,self.p.x,self.p.y,W*3,H*3)
+
+        pass
     pass
 class Attack(State):
+    def __init__(self,p):
+        pass
+    def enter(self,e):
+        pass
+    def exit(self,e):
+        pass
+    def do(self):
+        pass
+    def draw(self):
+        pass
     pass
 class Hurt(State):
+    def __init__(self,p):
+        pass
+    def enter(self,e):
+        pass
+    def exit(self,e):
+        pass
+    def do(self):
+        pass
+    def draw(self):
+        pass
     pass
 class Die(State):
+    def __init__(self,p):
+        pass
+    def enter(self,e):
+        pass
+    def exit(self,e):
+        pass
+    def do(self):
+        pass
+    def draw(self):
+        pass
     pass
 
 class Monster:
     def __init__(self):
-        self.x=800
-        self.y=80+H//2 *3
-        self.vx=0
-        self.face_dir=-1
+        self.x=random.randint(300,1000)
+        self.face_dir=random.choice([-1,1])
+        self.vx=MOVE_SPEED * self.face_dir
+        monster_half_h = (H * 3) // 2
+        self.y = 161 + monster_half_h
         self.frame=0.0
 
         self.img_walk=load_image('res/monster1_walk.png')
