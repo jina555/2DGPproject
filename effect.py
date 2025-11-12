6ef69b095f410d320a45935d98a2b4df367a1e77
jina555 <kja390@tukorea.ac.kr>
2025-11-12 17:55:36 +0900

    무기 이펙트 class 뼈대 작성

--- effect.py
+++ effect.py
@@ -1,3 +1,11 @@
 from pico2d import *
 import game_world
 import game_framework
+
+class SwordEffect:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
