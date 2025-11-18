e4ff9b06b23d31b5dfa442c39da6436d258b2e3a
jina555 <kja390@tukorea.ac.kr>
2025-11-18 23:29:47 +0900

    포탈 추가

---
+++ portal.py
@@ -0,0 +1,17 @@
+from pico2d import *
+
+class Portal:
+    def __init__(self,x,y):
+        self.image=load_image('res2/portal.png')
+        self.x,self.y=x,y
+        self.w=self.image.w
+        self.h=self.image.h
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x,self.y)
+
+    def get_bb(self):
+        return self.x-50,self.y-20,self.x+50,self.y+20
