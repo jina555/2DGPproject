1a2d0dce027f09f88cffaf833bb11ef49750205d
jina555 <kja390@tukorea.ac.kr>
2025-11-20 02:00:47 +0900

    구출할 친구 기본 뼈대 작성

--- npc.py
+++ npc.py
@@ -2,3 +2,12 @@ from pico2d import *
 import game_world
 import game_framework
 
+class Friend: # 구출 대상
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
+    def handle_event(self):
+        pass
