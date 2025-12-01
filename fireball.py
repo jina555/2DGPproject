00f3925a8e57fe956eebf85d032db91bbcd6bcd1
jina555 <kja390@tukorea.ac.kr>
2025-12-01 22:41:08 +0900

    보스2 attack class 뼈대 생성

--- fireball.py
+++ fireball.py
@@ -3,13 +3,30 @@ import game_world
 import game_framework
 
 class Fireball:
-    def __init__(self):
+    image=None
+    def __init__(self,x,y,face_dir):
+        if Fireball.image==None:
+            Fireball.image=load_image("res2/fireball.png")
+        self.x,self.y=x,y
+        self.dir=face_dir
+        self.velocity=800 * face_dir
+        self.width,self.height=50,50
         pass
     def update(self):
+        self.x += self.velocity * game_framework.frame_time
+
+        if self.x<0 or self.x > 1600:
+            game_world.remove_object(self)
+
         pass
     def draw(self):
+        self.image.draw(self.x,self.y,self.width,self.height)
+        draw_rectangle(*self.get_bb(),255,0,0)
         pass
     def get_bb(self):
+        return self.x-20,self.y-20,self.x+20,self.y+20
         pass
-    def handle_collision(self):
+    def handle_collision(self,group,other):
+        if group=='monster_attack:player':
+            game_world.remove_object(self)
         pass
