d486080b2bdf285324e3df1ff814d2b1be46c1b1
jina555 <kja390@tukorea.ac.kr>
2025-12-09 19:11:32 +0900

    엔딩모드 추가

---
+++ ending_mode.py
@@ -0,0 +1,14 @@
+from pico2d import *
+import game_framework
+import start_mode
+
+def init():
+    pass
+def finish():
+    pass
+def update():
+    pass
+def draw():
+    pass
+def handle_events():
+    pass
